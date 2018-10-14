f1cb96feb01dd06eda1849e95a70d98420696021
Carlos <mrusuario0001@gmail.com>
2018-10-14 15:52:57 -0500

    practice number 2 of angular 6
    practice number 2 of angular 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pract02';
+  name: string = '';
+
+  onClearField() {
+    this.name = '';
+  }
 }
